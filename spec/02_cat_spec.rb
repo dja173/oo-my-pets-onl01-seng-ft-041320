102776618d90c70a86a2c4bc3216d7905edba40a
dustin <djarcene@gmail.com>
2020-04-30 21:52:52 +0000

    Automatically backed up by Learn

--- spec/02_cat_spec.rb
+++ spec/02_cat_spec.rb
@@ -2,27 +2,46 @@ require_relative 'spec_helper.rb'
 
 describe Cat do
 
-  let(:cat) { Cat.new("crookshanks") }
-
-  it "can initialize a cat" do
-    expect(cat).to be_a(Cat)
+  before do
+    @owner = Owner.new("Hermione")
+    @cat = Cat.new("Crookshanks", @owner)
   end
 
-  it "initializes with a name" do
-    expect(cat.name).to eq("crookshanks")
+  after do
+    Owner.all.clear
   end
 
-  it "can't change its name" do 
-    expect { cat.name = "mr. whiskers" }.to raise_error
+  context 'Instance methods' do
+    it "initializes with a name and an Owner" do
+      expect(@cat.name).to eq("Crookshanks")
+      expect(@cat.owner.name).to eq("Hermione")
+    end
+
+    it "can change its owner" do
+      new_owner =  Owner.new("Harry")
+      @cat.owner = new_owner
+
+      expect(@cat.owner.name).to eq("Harry")
+    end
+
+    it "can't change its name" do
+      expect { @cat.name = "Mr. Whiskers" }.to raise_error NoMethodError
+    end
+
+    it "initializes with a nervous mood" do
+      expect(@cat.mood).to eq("nervous")
+    end
+
+    it "can change its mood" do
+      @cat.mood = "playful"
+      expect(@cat.mood).to eq("playful")
+    end
   end
 
-  it "initializes with a nervous mood" do
-    expect(cat.mood).to eq("nervous")
+  describe "Class methods" do
+    it "knows all the cats" do
+      expect(Cat.all).to include(@cat)
+      expect(Cat.all.count).to eq(6)
+    end
   end
-
-  it "can change its mood" do 
-    cat.mood = "playful"
-    expect(cat.mood).to eq("playful")
-  end
-  
 end
