bc9e91be5c01447b4d2d5217c0d5bd59a63cf803
dustin <djarcene@gmail.com>
2020-04-30 21:53:16 +0000

    Automatically backed up by Learn

--- spec/03_dog_spec.rb
+++ spec/03_dog_spec.rb
@@ -2,27 +2,45 @@ require_relative 'spec_helper.rb'
 
 describe Dog do
 
-  let(:dog) { Dog.new("Daisy") }
-
-  it "can initialize a dog" do
-    expect(dog).to be_a(Dog)
+  before do
+    @owner = Owner.new("Timmy")
+    @dog = Dog.new("Lassie", @owner)
   end
 
-  it "initializes with a name" do
-    expect(dog.name).to eq("Daisy")
+  after do
+    Owner.all.clear
   end
 
-  it "can't change its name" do 
-    expect { dog.name = "Fido" }.to raise_error
+  context 'Instance methods' do
+    it "initializes with a name and an Owner" do
+      expect(@dog.name).to eq("Lassie")
+      expect(@dog.owner.name).to eq("Timmy")
+    end
+
+    it "can change its owner" do
+      new_owner =  Owner.new("Jane")
+      @dog.owner = new_owner
+
+      expect(@dog.owner.name).to eq("Jane")
+    end
+
+    it "can't change its name" do
+      expect { @dog.name = "Fido" }.to raise_error NoMethodError
+    end
+
+    it "initializes with a nervous mood" do
+      expect(@dog.mood).to eq("nervous")
+    end
+
+    it "can change its mood" do
+      @dog.mood = "hungry"
+      expect(@dog.mood).to eq("hungry")
+    end
   end
 
-  it "initializes with a nervous mood" do
-    expect(dog.mood).to eq("nervous")
+  describe "Class methods" do
+    it "knows all the dogs" do
+      Dog.all.include?(@dog)
+    end
   end
-
-  it "can change its mood" do 
-    dog.mood = "hungry"
-    expect(dog.mood).to eq("hungry")
-  end
-  
 end
